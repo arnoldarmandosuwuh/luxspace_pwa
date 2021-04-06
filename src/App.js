db6fe29097cff231fdf1965b67ba1fcc05331328
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2021-04-07 01:47:20 +0700

    offline info and manifest

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ import Clients from "./components/Clients";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Offline from "./components/Offline";
 
 const App = () => {
   const [items, setItems] = useState([]);
+
+  const [offlineStatus, setOfflineStatus] = useState(!navigator.onLine);
+
+  const handleOfflineStatus = () => {
+    setOfflineStatus(!navigator.onLine);
+  };
 
   useEffect(() => {
     (async () => {
@@ -24,10 +31,25 @@ const App = () => {
       );
       const { nodes } = await response.json();
       setItems(nodes);
+
+      const script = document.createElement("script");
+      script.src = "/carousel.js";
+      script.async = false;
+      document.body.appendChild(script);
     })();
-  }, []);
+
+    handleOfflineStatus();
+    window.addEventListener("online", handleOfflineStatus);
+    window.addEventListener("offline", handleOfflineStatus);
+
+    return () => {
+      window.removeEventListener("online", handleOfflineStatus);
+      window.removeEventListener("offline", handleOfflineStatus);
+    };
+  }, [offlineStatus]);
   return (
     <>
+      {offlineStatus && <Offline />}
       <Header />
       <Hero />
       <Browse />
